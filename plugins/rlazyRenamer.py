c795bbfabaec773329527fb7899eca1968bf499f
MarselDeveloper <141441864+marsel7official@users.noreply.github.com>
2023-09-05 07:50:38 +0530

    Update rlazyRenamer.py

--- plugins/rlazyRenamer.py
+++ plugins/rlazyRenamer.py
@@ -47,6 +47,48 @@ async def rename_start(client, message):
             filesize = humanize.naturalsize(file.file_size) 
             filename = file.file_name
             text = f"""\n⨳ *•.¸♡ L҉ΛＺ𝐲 ＭⓄｄ𝓔 ♡¸.•* ⨳\n\n**Please tell, what should i do with this file.?**\n\n**🎞File Name** :- `{filename}`\n\n⚙️**File Size** :- `{filesize}`"""
+            buttons = [[ InlineKeyboardButton("📝 Start Renaming 📝", callback_data="rename") ],
+                           [ InlineKeyboardButton("✖️ Close ✖️", callback_data="cancel") ]]
+            await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+
+        elif message.from_user.id in LAZY_RENAMERS :
+            file = getattr(message, message.media.value)
+            filesize = humanize.naturalsize(file.file_size) 
+            filename = file.file_name
+            try:
+                text = f"""\n⨳ *•.¸♡ L҉ΛＺ𝐲 ＭⓄｄ𝓔 ♡¸.•* ⨳\n\n**Please tell, what should i do with this file.?**\n\n**🎞File Name** :- `{filename}`\n\n⚙️**File Size** :- `{filesize}`"""
+                buttons = [[ InlineKeyboardButton("📝 Start Renaming 📝", callback_data="rename") ],
+                           [ InlineKeyboardButton("✖️ Close ✖️", callback_data="cancel") ]]
+                await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+                await sleep(FLOOD)
+            except FloodWait as e:
+                await sleep(e.value)
+                text = f"""\n⨳ *•.¸♡ L҉ΛＺ𝐲 ＭⓄｄ𝓔 ♡¸.•* ⨳\n\n**Please tell, what should i do with this file.?**\n\n**🎞File Name** :- `{filename}`\n\n⚙️**File Size** :- `{filesize}`"""
+                buttons = [[ InlineKeyboardButton("📝 Start Renaming 📝", callback_data="rename") ],
+                           [ InlineKeyboardButton("✖️ Close ✖️", callback_data="cancel") ]]
+                await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+            except:
+                pass
+        else:
+            file = getattr(message, message.media.value)
+            filesize = humanize.naturalsize(file.file_size) 
+            filename = file.file_name
+            text = f"""\n⨳ *•.¸♡ L҉ΛＺ𝐲 ＭⓄｄ𝓔 ♡¸.•* ⨳\n\n**Please tell, what should i do with this file.?**\n\n**🎞File Name** :- `{filename}`\n\n⚙️**File Size** :- `{filesize}`"""
+            buttons = [[ InlineKeyboardButton("📝 Start Renaming 📝", callback_data="rename") ],
+                           [ InlineKeyboardButton("✖️ Close ✖️", callback_data="cancel") ]]
+            await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
+    else:
+        return
+# Born to make history @LazyDeveloper !
+
+@Client.on_message( filters.private & (filters.document | filters.audio | filters.video))
+async def rename_start(client, message):
+    if (LAZY_MODE==True):
+        if message.from_user.id in ADMINS :
+            file = getattr(message, message.media.value)
+            filesize = humanize.naturalsize(file.file_size) 
+            filename = file.file_name
+            text = f"""\n⨳ *•.¸♡ L҉ΛＺ𝐲 ＭⓄｄ𝓔 ♡¸.•* ⨳\n\n**Please tell, what should i do with this file.?**\n\n**🎞File Name** :- `{filename}`\n\n⚙️**File Size** :- `{filesize}`"""
             buttons = [[ InlineKeyboardButton("📝✧✧ S𝚝ar𝚝 re𝚗aᗰi𝚗g ✧✧📝", callback_data="rename") ],
                        [ InlineKeyboardButton("⨳  C L Ф S Ξ  ⨳", callback_data="cancel") ]]
             await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
